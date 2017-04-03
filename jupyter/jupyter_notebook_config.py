60a6b5102df1e58f097a4231c8362992da40d2f5
Andreas Sorge <as@pycnic.org>
2017-04-03 10:27:25 +0200

    Jupyter: post_save to script

--- jupyter/jupyter_notebook_config.py
+++ jupyter/jupyter_notebook_config.py
@@ -535,6 +535,17 @@
 #  - path: the filesystem path to the file just written - model: the model
 #  representing the file - contents_manager: this ContentsManager instance
 #c.FileContentsManager.post_save_hook = None
+import os
+from subprocess import check_call
+
+def post_save(model, os_path, contents_manager):
+    """post-save hook for converting notebooks to .py scripts"""
+    if model['type'] != 'notebook':
+        return # only do this for notebooks
+    d, fname = os.path.split(os_path)
+    check_call(['jupyter', 'nbconvert', '--to', 'script', fname], cwd=d)
+
+c.FileContentsManager.post_save_hook = post_save
 
 ## 
 #c.FileContentsManager.root_dir = ''
